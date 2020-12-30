4e847eeadc48678fe58ee0ade94fd56bfd6fc48c
dxk5271 <dannyking5271@gmail.com>
2020-12-30 10:59:08 -0500

    cleaned up, ready to add sql nsert if/loop

--- baseball_db.py
+++ baseball_db.py
@@ -6,73 +6,66 @@ from bs4 import BeautifulSoup
 from teams import *
 import pyodbc 
 
-
-
-my_list = []
-my_list = pd.Series(my_list)
-i = 0
-columns_names = ['AB','Hits','2B','3B','HR','RBI','BB','K', 'AVG','OBP','SLG','OPS','SlG','OPS']
-df = pd.DataFrame(columns = columns_names)
-for j in range (30):
+def web_scrape():
+    i = 0
     
- 
-    
-    x = 1 + i
-    x = str(x)
-    print("LOOK HERE")
-    print(x)
-    url = 'https://www.espn.com/mlb/player/batvspitch/_/id/35201/teamId/' + x
-    print(url)
-    print("URL ^")
-    page = requests.get(url)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    
-    data = soup.find('td', attrs = {'class': 'Table__TD'})
-    
-    data = data.findNext('td')
-    data_print = data.get_text()
-    #print(type(data_print))
-    print(data_print)
-    if data_print in teams:
-        print("BROKE")
-        i = i+1
-        continue
-    
-    
-    # =============================================================================
-    # for x in range(10):
-    #     #data2 = data.findNext('td').get_text()
-    #     data2 = data.findNext('td')
-    #     print(data2)
-    # =============================================================================
- 
-
-
-    
-    
-    o = 1
-    it = 0 
-    conn = pyodbc.connect('Driver={SQL Server};'
-                      'Server=DESKTOP-F46GKKA;'
-                      'Database=Baseball_data;'
-                      'Trusted_Connection=yes;')
-    cursor = conn.cursor()
-    for x in range(200):
-        it = it + 1
-        p= 0 + o
+    for j in range (30):
+        x = 1 + i
+        x = str(x)
+        print("LOOK HERE")
+        print(x)
+        url = 'https://www.espn.com/mlb/player/batvspitch/_/id/35201/teamId/' + x
+        print(url)
+        print("URL ^")
+        page = requests.get(url)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        
+        data = soup.find('td', attrs = {'class': 'Table__TD'})
+        
         data = data.findNext('td')
-        H = data.get_text()
+        data_print = data.get_text()
+        #print(type(data_print))
+        print(data_print)
+        if data_print in teams:
+            print("BROKE")
+            i = i+1
+            continue
         
-        if H == 'Totals':
-            break
         
-        print(H)
-        cursor.execute("INSERT INTO dbo.STATS$(H) VALUES (?)", (H))    
-        #Values = [H, AVG]
-        #cursor.execute(SQLCommand,Values)
-        conn.commit() 
-        print('committed')
+        # =============================================================================
+        # for x in range(10):
+        #     #data2 = data.findNext('td').get_text()
+        #     data2 = data.findNext('td')
+        #     print(data2)
+        # =============================================================================
+     
+    
+    
         
-        o = o+1
-    i = i +1
-    #df.append(my_list)
+        
+        o = 1
+        it = 0 
+        conn = pyodbc.connect('Driver={SQL Server};'
+                          'Server=DESKTOP-F46GKKA;'
+                          'Database=Baseball_data;'
+                          'Trusted_Connection=yes;')
+        cursor = conn.cursor()
+        for x in range(200):
+            it = it + 1
+            p= 0 + o
+            data = data.findNext('td')
+            H = data.get_text()
+            
+            if H == 'Totals':
+                break
+            
+            print(H)
+            cursor.execute("INSERT INTO dbo.STATS$(H) VALUES (?)", (H))    
+            #Values = [H, AVG]
+            #cursor.execute(SQLCommand,Values)
+            conn.commit() 
+            print('committed')
+            
+            o = o+1
+        i = i +1
+web_scrape()
